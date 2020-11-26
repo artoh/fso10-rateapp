881155c0cf5aff3c0ea4f63d3b3627b708a02c5b
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2020-11-26 16:30:10 +0200

    10.6 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,31 +1,42 @@
 import React from 'react';
-import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { View, StyleSheet, TouchableWithoutFeedback, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
+import {Link} from 'react-router-native';
 
 import Text from './Text';
 import theme from '../theme';
+
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     display: 'flex',    
     backgroundColor: theme.colors.appBar.background,    
-    // ...
   },
   caption: {
       color: theme.colors.appBar.foreground,
       fontWeight: theme.fontWeights.bold,
+      padding: 10
   }
-  // ...
 });
+
+const AppBarTab = (props) => {
+  return(
+    <Link to={props.to} component={TouchableWithoutFeedback}>
+      <Text style={styles.caption}>{props.title}</Text>
+    </Link>    
+  );
+};
+
 
 const AppBar = () => {
   return (
-    <TouchableWithoutFeedback>
         <View style={styles.container}>
-            <Text style={styles.caption}>Repositories</Text>
-        </View>
-    </TouchableWithoutFeedback>
+          <ScrollView horizontal>
+           <AppBarTab title="Repositories" to="/"/>
+           <AppBarTab title="Sign in" to="/sign" />         
+           </ScrollView>
+        </View>    
   );
 };
 
